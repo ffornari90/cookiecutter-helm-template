c19b1afd8e3e366effd0bdd11e56894bafd9439b
Federico Fornari <federico.fornari@ecmwf.int>
2024-10-24 16:41:45 +0200

    Updated post hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -121,7 +121,6 @@ def update_values_yaml(context):
 
     # Update external_secrets section if 'include_external_secret' is 'yes'
     if context.get('include_external_secret') == 'yes':
-        print(context.get('external_secret_data', {"data":[{"secret_key": "example-key-1","vault_secret_key": "vault/secret/data/path-1","vault_secret_property": "secret-property-1"},{"secret_key": "example-key-2","vault_secret_key": "vault/secret/data/path-2","vault_secret_property": "secret-property-2"}]}).get('data'))
         external_secret = {
             "name": context.get('external_secret_name', 'example-external-secret'),
             "target": {
@@ -134,7 +133,7 @@ def update_values_yaml(context):
                         "key": entry['vault_secret_key'],
                         "property": entry['vault_secret_property']
                     }
-                } for entry in context.get('external_secret_data', {"data":[{"secret_key": "example-key-1","vault_secret_key": "vault/secret/data/path-1","vault_secret_property": "secret-property-1"},{"secret_key": "example-key-2","vault_secret_key": "vault/secret/data/path-2","vault_secret_property": "secret-property-2"}]})
+                } for entry in context.get('external_secret_data', {"data":[{"secret_key": "example-key-1","vault_secret_key": "vault/secret/data/path-1","vault_secret_property": "secret-property-1"},{"secret_key": "example-key-2","vault_secret_key": "vault/secret/data/path-2","vault_secret_property": "secret-property-2"}]}).get('data')
             ]
         }
         values['externalSecrets'].append(external_secret)
